f7c4d4f5262791d830e28d698ec8af49342f1555
Angad-Godara <angadrrr2002@gmail.com>
2022-08-12 23:11:41 +0530

    ADD: added uploading bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { useState } from 'react'
 import axios from 'axios';
+import { Row, Col, ProgressBar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   const server = process.env.REACT_APP_SERVER;
 
   const [file, setfile] = useState(null)
   const [upload, setupload] = useState(false)
+  const [progress, setProgress] = useState(0)
 
   const onFileChange = (e) => {
     // console.log(e.target.files[0]);
@@ -22,17 +26,30 @@ function App() {
       'file',
       file)
     // console.log(formData);
-    axios.post(`${server}`, formData).then(response =>
+    axios.post(`${server}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress: data => {
+        //Set the progress value to show the progress bar
+        setProgress(Math.round((100 * data.loaded) / data.total))
+      },
+    }).then(response =>
       setupload(false)
     )
   }
 
   return (
     <div className="App">
-      <>
+      <div className="uploader">
         <input type='file' name='myfile' onChange={onFileChange} />
         <button disabled={upload} onClick={uploadit}>Upload</button>
-      </>
+      </div>
+      <Row>
+        <Col>
+          <ProgressBar animated style={{ margin: '20px' }} now={progress} label={`${progress}%`} />
+        </Col>
+      </Row>
     </div>
   );
 }
